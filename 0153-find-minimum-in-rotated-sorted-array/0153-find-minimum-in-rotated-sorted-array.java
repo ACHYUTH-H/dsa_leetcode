be95ef2da4bb8472575b8296f06f874964b6a04b
ACHYUTH-H <achyuthhalli@gmail.com>
2026-07-30 16:34:44 +0530

    Time: 0 ms (100%), Space: 43.6 MB (88.15%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,48 +1,33 @@
 class Solution {
-    public int findMin(int[] nums) {
-         //now lets think how it works
-    int n=nums.length;
-    if(n==1)
+    public int findMin(int[] nums) 
     {
-        return nums[0];
-    }
-    int low=0;
-    int high=n-1;
-    int min=100000000;
-    while(low<=high)
-    {
-        int mid=(low+high)/2;
-        //first step check if left part of the array is sorted with mid
-        if(nums[low]<=nums[mid])
+        int low=0;
+        int high=nums.length-1;
+        int min=100000;
+        
+        while(low<=high)
         {
-            //min will be the starting element
-            min=Math.min(min,nums[low]);
-            if(min>nums[high])
+            if(nums[low]<=nums[high])
             {
-                //right part of array is having the minimum
-                //move left to right
+                //entire array is sorted
+                return Math.min(min,nums[low]);
+            }
+            int mid=(low+high)/2;
+            //now we are checking if left part is sorted
+            if(nums[low]<=nums[mid])
+            {
+                min=Math.min(min,nums[low]);
                 low=mid+1;
             }
-            else
-            {
-                high=mid-1;
-            }
-        }
-        //right part is sorted check if we can get the least element
-        else
-        {
-            if(nums[mid+1]<=nums[mid])
-            {
-                min=Math.min(min,nums[mid+1]);
-                low=mid+1;            }
+            //now right part is sorted
             else
             {
                 min=Math.min(min,nums[mid]);
                 high=mid-1;
-            }
 
+            }
         }
-    } 
-       
-       return min;  }
+        return min;
+        
+    }
 }
