e79b04c8021c4834ffae3a2d7d17b367ccd39080
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-02 10:04:27 +0530

    Time: 19 ms (38.86%), Space: 82.9 MB (13.16%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,46 +1,78 @@
 class Solution {
-
-    // Return the number of maximum bouquets that can be made on day mid.
-    private int getNumOfBouquets(int[] bloomDay, int mid, int k) {
-        int numOfBouquets = 0;
-        int count = 0;
-
-        for (int i = 0; i < bloomDay.length; i++) {
-            // If the flower is bloomed, add to the set. Else reset the count.
-            if (bloomDay[i] <= mid) {
-                count++;
-            } else {
-                count = 0;
+    public boolean isPossible(int[]arr,int day,int m,int k)
+    {
+        int count= 0;
+        int bouquets=0;
+        for(int i:arr)
+        {
+            if(i<=day)
+            {
+                count++;//consecutive numbers increment
+                if(count==k)
+                {
+                    count=0;
+                    bouquets++;
+                }
             }
-
-            if (count == k) {
-                numOfBouquets++;
-                count = 0;
+            else  //no consecutive numbers 
+            {
+                count=0;
             }
         }
+        return bouquets>=m;
 
-        return numOfBouquets;
     }
-
-    public int minDays(int[] bloomDay, int m, int k) {
-        int start = 0;
-        int end = 0;
-        for (int day : bloomDay) {
-            end = Math.max(end, day);
+    public int maxi(int[] arr)
+    {
+        int n=arr.length;
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            max=Math.max(max,arr[i]);
         }
+        return max;
+    }
+    public int mini(int[] arr)
+    {
+        int n=arr.length;
+        int min=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            min=Math.min(min,arr[i]);
+        }
+        return min;
+    }
+    public int minDays(int[] bloomDay, int m, int k) 
+    {
+        //m is number of boquteee
+        //k is number of adjacent flowers
+        //elements in bloomDay tells us how many days to wait for them
+        int totalflowers=m*k;
+        int bld=bloomDay.length;
+        if(totalflowers>bloomDay.length)
+        {
+            return -1;
+        }
+        //okay this is binary search problem where 
+        int max=maxi(bloomDay);
+        int min=mini(bloomDay);
+        int low=min;
+        int high=max;
+        int ans=-1;
 
-        int minDays = -1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (getNumOfBouquets(bloomDay, mid, k) >= m) {
-                minDays = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(isPossible(bloomDay,mid,m,k))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
             }
         }
-
-        return minDays;
+        return ans;
     }
 }
