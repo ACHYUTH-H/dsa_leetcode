d8f040bd338d2e512127a8675d1cb4a9d1140402
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-03 22:27:08 +0530

    Time: 5 ms (61.53%), Space: 44.2 MB (82.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,22 @@
 class Solution {
-    public boolean isAnagram(String s, String t) 
-    {
-        //to find if the words are anagram or not we need to check the occurances 
-        int n1=s.length();
-        int n2=t.length();
-        if(n1!=n2)
-        {
-            return false;
-        }
-        int [] freq=new int[26];
-        for(int i=0;i<26;i++)
-        {
-            freq[i]=0;
-        }
-        for(int i=0;i<n1;i++)
-        {
-            freq[s.charAt(i)-'a']++;
-        }
-        for(int i=0;i<n1;i++)
-        {
-            freq[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++)
-        {
-            if(freq[i]!=0)
-            {
-                return false;
-            }
-        }
-        return true;
 
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0)
+                return false;
+        }
+
+        return true;
     }
 }
