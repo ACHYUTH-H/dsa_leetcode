98d31dfd8dabb9e14028983b0ae844fa5160c072
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-04 20:13:02 +0530

    Time: 4 ms (54.79%), Space: 46.4 MB (63.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,27 +1,42 @@
 class Solution {
+    private int value (char c)
+    {
+        switch(c)
+        {
+            case 'I': return 1;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
     public int romanToInt(String s) 
     {
         //now we are converting the roman to int
         // okay if the smaller number comes before larger then just decrease the value from larger or else just add the larger
-        HashMap<Character,Integer> map=new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // map.put('I',1);
+        // map.put('V',5);
+        // map.put('X',10);
+        // map.put('L',50);
+        // map.put('C',100);
+        // map.put('D',500);
+        // map.put('M',1000);
+
         int ans=0;
         for(int i=0;i<s.length();i++)
         {
-            if(i<s.length()-1 && map.get(s.charAt(i))<map.get(s.charAt(i+1)) )
+            if(i<s.length()-1 && value(s.charAt(i))<value(s.charAt(i+1)) )
             {
-                ans=ans+(map.get(s.charAt(i+1))-map.get(s.charAt(i)));
+                ans=ans+(value(s.charAt(i+1))-value(s.charAt(i)));
                 i++;
             }
             else
             {
-                ans= ans + map.get(s.charAt(i));
+                ans= ans + value(s.charAt(i));
             }
         }
      return ans;   
