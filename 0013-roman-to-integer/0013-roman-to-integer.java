814a6a18815a1339db4dd496c43b4bb4f9492e6c
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-04 20:17:44 +0530

    Time: 3 ms (77.95%), Space: 46.5 MB (63.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,36 +1,44 @@
 class Solution {
-
-    private int value(char c){
-
-        switch(c){
-
+    private int value (char c)
+    {
+        switch(c)
+        {
             case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
             case 'D': return 500;
-            default: return 1000;
+            case 'M': return 1000;
         }
+        return 0;
     }
+    public int romanToInt(String s) 
+    {
+        //now we are converting the roman to int
+        // okay if the smaller number comes before larger then just decrease the value from larger or else just add the larger
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // map.put('I',1);
+        // map.put('V',5);
+        // map.put('X',10);
+        // map.put('L',50);
+        // map.put('C',100);
+        // map.put('D',500);
+        // map.put('M',1000);
 
-    public int romanToInt(String s){
-
-        int ans = 0;
-        int prev = 0;
-
-        for(int i=s.length()-1;i>=0;i--){
-
-            int curr = value(s.charAt(i));
-
-            if(curr < prev)
-                ans -= curr;
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(i<s.length()-1 && value(s.charAt(i))<value(s.charAt(i+1)) )
+            {
+                ans=ans+(value(s.charAt(i+1))-value(s.charAt(i)));
+                i++;
+            }
             else
-                ans += curr;
-
-            prev = curr;
+            {
+                ans= ans + value(s.charAt(i));
+            }
         }
-
-        return ans;
+     return ans;   
     }
 }
