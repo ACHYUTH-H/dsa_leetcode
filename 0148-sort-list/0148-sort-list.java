226774dfebd2d99314a4bb003a4c1de2617660a2
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-08 12:42:14 +0530

    Time: 9 ms (97.63%), Space: 59.3 MB (76.83%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,25 +9,74 @@
  * }
  */
 class Solution {
+    public ListNode middle(ListNode head)
+    {
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next !=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;//slow is where we return the node which is middle
+    }
+
+    public ListNode merge(ListNode left,ListNode right)
+    {
+        ListNode dummy=new ListNode(-1,null);
+        ListNode temp=dummy;
+        while(left!=null && right !=null)
+        {
+            if(left.val>right.val)
+            {
+                temp.next=right;
+                right=right.next;
+            }
+            else
+            {
+                temp.next=left;
+                left=left.next;
+            }
+            temp=temp.next;
+        }
+        if(left!=null)
+        {
+            temp.next=left;
+        }
+        else
+        {
+            temp.next=right;
+        }
+        return dummy.next;
+
+    }
+
+
+    public ListNode mergesort(ListNode head)
+    {
+        //now if the linked list is empty return empty
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+        ListNode mid=middle(head);
+        ListNode leftp=head;
+        ListNode rightp=mid.next;
+        mid.next=null;
+        leftp=mergesort(leftp);
+        rightp=mergesort(rightp);
+        return merge(leftp,rightp);
+
+
+
+    }
+
+
+
     public ListNode sortList(ListNode head) 
     {
-        ArrayList<Integer>arr=new ArrayList<>();
-        ListNode jai=head;
-        while(jai!=null)
-        {
-            arr.add(jai.val);
-            jai=jai.next;
-        }
-        //jai=null;
-        int i=0;
-        jai=head;
-        Collections.sort(arr);
-        while(i<arr.size())
-        {
-            jai.val=arr.get(i);
-            jai=jai.next;
-            i++;
-        }
-        return head;
+    //first send this to other function where u can find the mid value and do recursion
+    ListNode merged= mergesort(head);   
+    return merged; 
     }
 }
