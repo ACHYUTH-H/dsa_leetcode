725673e1ec519ef41283f3a33b6cbc139d880706
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-19 02:06:54 +0530

    Time: 815 ms (5.04%), Space: 80.1 MB (5.25%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countPrimes(int n) 
+    {
+        //now we know till which vlue we need to check
+        if(n<=2)return 0;
+        boolean[]nums=new boolean[n];
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+            nums[i]=true;
+        }
+        for(int i=2;i*i<n;i++)
+        {
+            if(nums[i]==true)
+            {
+                 //count++;
+                 int j=i*i;
+               while(j<n)
+               {
+                nums[j]=false;
+                j=j+i;
+               }
+               
+                //if it is prime then mark all of the numbers which is multiple to zero
+            }
+        }
+        //now we will have 1 in arr which tells us prime number
+        for(int i=2;i<n;i++)
+        {
+            if(nums[i]==true)
+            count++;
+        }
+        return count;
+        
+    }
+}
