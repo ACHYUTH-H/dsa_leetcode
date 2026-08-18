8cde140f0aecb0cd8ff332c0da5a604502425904
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-19 02:12:01 +0530

    Time: 630 ms (5.04%), Space: 79.4 MB (5.25%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,33 +1,27 @@
 class Solution {
-    public int countPrimes(int n) 
-    {
-        //now we know till which vlue we need to check
-        if(n<=2)return 0;
-        boolean[]nums=new boolean[n];
-        int count=0;
-       
-        for(int i=2;i*i<n;i++)
-        {
-            if(!nums[i])
-            {
-                 //count++;
-                 int j=i*i;
-               while(j<n)
-               {
-                nums[j]=true;
-                j=j+i;
-               }
-               
-                //if it is prime then mark all of the numbers which is multiple to zero
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+        
+        // Using a boolean array is faster and uses less memory than an int array
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        
+        // Loop only up to sqrt(n)
+        for (int i = 2; i * i < n; i++) {
+            if (!notPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    notPrime[j] = true;
+                }
             }
         }
-        //now we will have 1 in arr which tells us prime number
-        for(int i=2;i<n;i++)
-        {
-            if(!nums[i])
-            count++;
-        }
-        return count;
         
+        // Count remaining primes
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
