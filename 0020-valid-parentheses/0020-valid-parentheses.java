28f16072da71d54f368b0129a5587a58fe45d349
ACHYUTH-H <achyuthhalli@gmail.com>
2026-08-19 09:11:54 +0530

    Time: 5 ms (13.16%), Space: 43.4 MB (41.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) 
+    {
+        Stack<Character>stack=new Stack<>();
+        int n= s.length();
+        for(int i=0;i<n;i++)
+        {
+            char c=s.charAt(i);
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
+            {
+                stack.push(s.charAt(i));
+            }
+            else if (s.charAt(i)==')'||s.charAt(i)=='}'||s.charAt(i)==']')
+            { if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                char top = stack.peek();
+
+                if(((c==')')&&stack.peek()=='(') || ((c=='}')&&stack.peek()=='{') || ((c==']')&&stack.peek()=='['))
+                stack.pop();
+                else
+                return false;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        if(stack.isEmpty())
+        {
+            return true;
+        }
+        return false;
+    }
+}
