7b24a4d828bc5faa76fd68ff5bfb639ab3ea3686
Achyuth <achyuthhalli@gmail.com>
2026-08-31 10:11:04 +0000

    Time: 4 ms (74.11%), Space: 45.4 MB (36.4%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,26 @@
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) 
-    {
-     //as per the hint we got to know that we need to traverse from left to right 
-     //okay we need to see the nums2 and mark the pos    
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
-     int n=nums2.length;
-     int [] nextGreater=new int[10001];
-     Stack<Integer>st=new Stack<>();
-     for(int i= n-1;i>=0;i--)
-     {
-        while(!st.isEmpty() && st.peek()<=nums2[i])
-        {
-            st.pop();
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        // 1. Process nums2 right-to-left to build next greater map
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                st.pop();
+            }
+            nextGreater.put(nums2[i], st.isEmpty() ? -1 : st.peek());
+            st.push(nums2[i]);
         }
-        nextGreater[nums2[i]]=st.isEmpty()?-1:st.peek();
-        st.push(nums2[i]);
-     }
-     for(int i =0;i<nums1.length;i++)
-     {
-        nums1[i]=nextGreater[nums1[i]];
-     }
-     return nums1;
+
+        // 2. Map answers directly to nums1
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = nextGreater.get(nums1[i]);
+        }
+
+        return nums1;
     }
 }
