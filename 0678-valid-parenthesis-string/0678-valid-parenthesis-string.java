507759409cb21ea0a997372b88d70e4a32e60ead
Achyuth <achyuthhalli@gmail.com>
2026-09-14 06:48:03 +0000

    Time: 0 ms (100%), Space: 42.6 MB (68.24%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkValidString(String s) 
+    {
+        int min=0;
+        int max=0;
+    for(int i =0;i<s.length();i++)
+    {
+        if(s.charAt(i)=='(')
+        {
+            min++;
+            max++;
+        }
+        else if(s.charAt(i)==')')
+        {
+            min--;
+            max--;
+        }
+
+        else
+        {
+            min--;
+            max++;
+        }
+
+        min=Math.max(0,min);
+         if(max<0)
+        {
+        return false;
+        }
+    
+    }    
+
+   return min==0;
+    }
+}
